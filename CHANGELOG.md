74b672c34ea6d026d0c1da885b3ffad735a53089
Joshua Onyenwere <joshua.onyenwere@bike24.net>
2024-02-11 13:46:18 +0100

    VOIDS-002 Application configurations completed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+[v0.0.2] - 11.01.2024
+
+    Install Class validator and transformer dependencies for BE
+    Add Supply resource in BE
+    Add MUI theme to centralise styling 
+
 [v0.0.1] - 11.01.2024
 
     Application setup for backend(be) and frontend(fe). 
--- voids-be/src/app.module.ts
+++ voids-be/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupplyModule } from './supply/supply.module';
 
 @Module({
-  imports: [],
+  imports: [SupplyModule],
   controllers: [AppController],
   providers: [AppService],
 })
